99e178d798663e4aa8c26a5d3d82194c34fc6b94
shyamgms <92625721+shyamgms@users.noreply.github.com>
2022-08-17 21:28:25 +0530

    Assignment 1 :BE

--- Backend_nodejs_express/node-express/index.js
+++ Backend_nodejs_express/node-express/index.js
@@ -1,9 +1,12 @@
-const express = require('express'),
-     http = require('http');
-     morgan = require('morgan');
-     bodyParser = require('body-parser');
+const express = require('express');
+const http = require('http');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
+
 const hostname = 'localhost';
 const port = 3000;
 
@@ -11,38 +14,20 @@ const app = express();
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-
-
-app.get('/dishes/:dishId', (req,res,next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req,res,next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req,res,next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req,res,next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
-
 app.use('/dishes', dishRouter);
-app.use(express.static(__dirname+'/public'));
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'text/html');
+   res.end('<html lang="en"><body><h1>This is an Express Server</h1></body></html>');
 });
 
 const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
--- Backend_nodejs_express/node-express/routes/dishRouter.js
+++ Backend_nodejs_express/node-express/routes/dishRouter.js
@@ -29,4 +29,22 @@ dishRouter.route('/')
     res.end('Deleting all the dishes');
 });
 
+dishRouter.get('/:dishId', (req,res,next) => {
+    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
+})
+
+.post('/:dishId', (req,res,next) => {
+    res.statusCode = 403;
+    res.end('POST operation not supported on /dishes' + req.params.dishId);
+})
+
+.put('/:dishId', (req,res,next) => {
+    res.write('Updating the dish: ' + req.params.dishId + '\n');
+    res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+})
+
+.delete('/:dishId', (req,res,next) => {
+    res.end('Deleting dish: ' + req.params.dishId);
+});
+
 module.exports = dishRouter;

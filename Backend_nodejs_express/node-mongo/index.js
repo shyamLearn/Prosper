5c315bf7c21d7524efec4d99d85e20f4f9fca6a1
shyamgms <92625721+shyamgms@users.noreply.github.com>
2022-08-21 14:48:08 +0530

    Node MongoDb exp-2

--- Backend_nodejs_express/node-mongo/index.js
+++ Backend_nodejs_express/node-mongo/index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,26 +12,51 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection("dishes");
-    collection.insertOne({"name": "Uthappizza", "description": "test"},
-    (err, result) => {
-        assert.equal(err,null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test"},
+    "dishes", (result) => {
+        console.log("Insert Document:\n", result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err,null);
-            
-            console.log("Found:\n");
-            console.log(docs);
+        dboper.findDocuments(db, "dishes", (docs) => {
+            console.log("Found Documents:\n", docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+            dboper.updateDocument(db, { name: "Vadonut" },
+                { description: "Updated Test" }, "dishes",
+                (result) => {
+                    console.log("Updated Document:\n", result.result);
 
-                client.close();
-            });
+                    dboper.findDocuments(db, "dishes", (docs) => {
+                        console.log("Found Updated Documents:\n", docs);
+                        
+                        db.dropCollection("dishes", (result) => {
+                            console.log("Dropped Collection: ", result);
+
+                            client.close();
+                        });
+                    });
+                });
         });
-    });
+});
+    // const collection = db.collection("dishes");
+    // collection.insertOne({"name": "Uthappizza", "description": "test"},
+    // (err, result) => {
+    //     assert.equal(err,null);
+
+    //     console.log("After Insert:\n");
+    //     console.log(result.ops);
+
+    //     collection.find({}).toArray((err, docs) => {
+    //         assert.equal(err,null);
+            
+    //         console.log("Found:\n");
+    //         console.log(docs);
+
+    //         db.dropCollection("dishes", (err, result) => {
+    //             assert.equal(err,null);
+
+    //             client.close();
+    //         });
+    //     });
+    // });
 
 });

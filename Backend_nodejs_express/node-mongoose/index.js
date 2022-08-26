bd6113399941c7d21d81b7de3d76fcdecc81f476
shyamgms <92625721+shyamgms@users.noreply.github.com>
2022-08-26 22:20:04 +0530

    mongoose part-2

--- Backend_nodejs_express/node-mongoose/index.js
+++ Backend_nodejs_express/node-mongoose/index.js
@@ -8,12 +8,37 @@ const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log('Connected correctly to server');
 
-    var newDish = Dishes({
+    //First way of saving the dishes
+
+    // var newDish = Dishes({
+    //     name: 'Uthappizza',
+    //     description: 'test'
+    // });
+
+    // newDish.save()
+    //     .then((dish) => {
+    //         console.log(dish);
+
+    //         return Dishes.find({}).exec();
+    //     })
+    //     .then((dishes) => {
+    //         console.log(dishes);
+
+    //         return Dishes.remove({});
+    //     })
+    //     .then(() => {
+    //         return mongoose.connection.close();
+    //     })
+    //     .catch((err) => {
+    //         console.logg(err);
+    //     })
+    // 
+    // Different approach
+
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
-    });
-
-    newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
 

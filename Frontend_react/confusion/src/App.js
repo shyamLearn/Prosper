c19cb14037553ab73e342cc8dd322b980166abac
shyamgms <92625721+shyamgms@users.noreply.github.com>
2022-09-19 22:29:11 +0530

    components react

--- Frontend_react/confusion/src/App.js
+++ Frontend_react/confusion/src/App.js
@@ -1,19 +1,29 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Navbar, NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+  render() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color = "primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu/>
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+}
 }
 
 export default App;

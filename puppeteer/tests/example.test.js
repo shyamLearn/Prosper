3a6efe50ec2ce432924512417d38a5cb183ba414
shyamgms <shyamg@tericsoft.com>
2023-02-07 17:45:31 +0530

    added few settings

--- puppeteer/tests/example.test.js
+++ puppeteer/tests/example.test.js
@@ -1,8 +1,12 @@
 const puppeteer = require('puppeteer')
 
-describe('My Fist Puppeteer Test', () =>{
-    it('should launch the browser', async function() {
-        const browser = await puppeteer.launch({ headless: false, executablePath: '/usr/bin/chromium-browser'})
+describe('My Fist Puppeteer Test', () => {
+    it('should launch the browser', async function () {
+        const browser = await puppeteer.launch({ 
+            headless: false, 
+            executablePath: '/usr/bin/chromium-browser', 
+            slowMo: 50, 
+            devtools: true })
         const page = await browser.newPage()
         await page.goto('http://example.com/')
         await page.waitForSelector('h1')
